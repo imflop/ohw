28c61231c68397c4a2ff6bf1f06db2b047f7c678
fl <imflop@gmail.com>
2020-05-28 23:31:13 +0300

    HW1 is completed (#1)
    * HW1 is completed
    
    * Fixed broken go get url
    
    * Fixed broken go get url, again
    
    * Fixed broken go get url, and again
    
    * Fixed broken go get url, hmmmmm
    
    * Fixed broken go get url, (╯°□°）╯︵ ┻━┻
    
    * Fixed broken go get url, (╯°□°）╯︵ ┻━┻
    
    * (╯°□°）╯︵ ┻━┻

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
+// Const ...
+const (
+	CustomFormat = "2006-01-02 15:04:05 -0700 MST"
+	AppleNTPAddr = "time.apple.com"
+)
+
 func main() {
-	// Place your code here
+	localTime := time.Now().Format(CustomFormat)
+	ntpTime, err := ntp.Time(AppleNTPAddr)
+	if err != nil {
+		log.Fatalf("Error while getting time from %s: %s", AppleNTPAddr, err)
+	}
+	roundedNtpTime := ntpTime.Round(ntpTime.Sub(ntpTime)).Format(CustomFormat)
+	fmt.Printf("current time: %v\nexact time: %v\n", localTime, roundedNtpTime)
 }
