5961c3abc92dd695a135a6e05d049ca5ea2a2dfc
imflop <imflop@gmail.com>
2020-07-01 21:39:29 +0300

    Работа над частотным поиском

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,44 @@
 package hw03_frequency_analysis //nolint:golint,stylecheck
 
-func Top10(_ string) []string {
-	// Place your code here
+import (
+	"fmt"
+	"regexp"
+)
+
+func getWords(text string) []string {
+	words := regexp.MustCompile(`\pL+('\pL+)*`)
+	return words.FindAllString(text, -1)
+}
+
+func countWords(words []string) map[string]int {
+	m := make(map[string]int)
+	for _, word := range words {
+		m[word]++
+	}
+	return m
+}
+
+//func sortData(data map[string]int) map[string]int {
+//	sorted := make(map[string]int)
+//	values := make([]int, 0, len(data))
+//	for _, v := range data {
+//		values = append(values, v)
+//	}
+//	sort.Ints(values)
+//	for _, v := range values {
+//		fmt.Println(v)
+//	}
+//	return sorted
+//}
+
+func Top10(s string) []string {
+	if len(s) == 0 {
+		return nil
+	}
+	//var result []string
+	data := countWords(getWords(s))
+	for i := 0; i < 10; i++ {
+	}
+	fmt.Println(data)
 	return nil
 }
