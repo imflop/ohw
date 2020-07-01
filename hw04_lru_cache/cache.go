89f56a0c2fa03f7e123f86d87b65d211f38f88db
imflop <imflop@gmail.com>
2020-07-01 21:43:36 +0300

    Реализация листа

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,9 +1,14 @@
 package hw04_lru_cache //nolint:golint,stylecheck
 
+import "github.com/imflop/ohw/hw04_lru_cache/list"
+
 type Key string
 
 type Cache interface {
 	// Place your code here
+	Set(key string, value interface{}) bool
+	Get(key string) (interface{}, bool)
+	Clear()
 }
 
 type lruCache struct {
@@ -11,10 +16,15 @@ type lruCache struct {
 	// - capacity
 	// - queue
 	// - items
+	capacity int
+	queue *list.List
+	items map[int]*list.listItem
 }
 
 type cacheItem struct {
 	// Place your code here
+	key string
+	value int
 }
 
 func NewCache(capacity int) Cache {
